af7bb1543125b7a40721db7852dae92f093ee6ed
Anisur Rahman <anis.dapunia@gmail.com>
2023-10-03 21:30:15 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Share/Navbar/Navbar";
+import Footer from "../Share/Footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Share/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>footer is cooming soom</div>;
+};
+
+export default Footer;
